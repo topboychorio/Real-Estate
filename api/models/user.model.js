8d76e821f4944928f564d468661fdd1218410eb8
topboychorio <choriothesecond@gmail.com>
2024-01-12 14:09:16 +0300

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9]*$/, // Only allow letters and numbers
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      match: /^[a-zA-Z0-9]*$/, // Only allow letters and numbers
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
