1ff28224e48bd7c30c82c034d674bfa476cfdfac
topboychorio <choriothesecond@gmail.com>
2024-02-01 16:03:27 +0300

    create listing api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -26,6 +26,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
